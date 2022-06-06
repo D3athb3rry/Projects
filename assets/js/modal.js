efdc6733f2ab036fc41687d27a6678b80c6d54fa
Deathberry <polkhovskij.dmitrij@gmail.com>
2022-06-06 08:58:40 +0300

    update modal.js

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -23,3 +23,74 @@ window.onclick = function(event) {
       modal.classList.remove('show');
     }
 }
+
+
+function insertAfter(referenceNode, newNode) 
+{
+     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+function createText() 
+{
+     var selectValue = document.getElementById("ticket-type").value;
+     if (selectValue == "permanent") 
+     {
+         var text = "Permanent exhibition";
+     } 
+     else if (selectValue == "temporary") {
+         text = "Temporary exhibition";
+     }
+     else if (selectValue == "combined") {
+        text = "Combined Admission";
+    }
+    else if (selectValue == "") {
+        text = "Ticket Type";
+    }
+     document.getElementById("type-option").innerText = text;
+}
+createText();
+
+function createTime(time) {
+    if (time.value !== "") {
+        var hours = time.split(":")[0];
+        var minutes = time.split(":")[1];
+        var displayTime = hours + ":" + minutes;
+        document.getElementById("time-option").innerHTML = displayTime;
+    }
+}
+
+function createDate(date) {
+    var allMonth = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December' 
+      ];
+      
+      var days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ];
+    var dateInput = document.getElementById( 'date' ).value;
+    if (dateInput !== "") {
+    var d = new Date(dateInput);
+    var month = d.getMonth();
+    var date = d.getDate();
+    var day = d.getDay();
+    var displayDate = days[day] + ', ' + allMonth[month] + " " + date;
+    document.getElementById("date-option").innerHTML = displayDate;
+    }
+}
